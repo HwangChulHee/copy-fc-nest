7d328d71001313f8793672eb8084ce654a5490d7
HwangChulHee <kum15479@naver.com>
2024-07-04 16:57:04 +0900

    4-9 configModule을 통한 환경변수 설정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,17 +7,22 @@ import {
   Query,
 } from '@nestjs/common';
 import { AppService } from './app.service';
-import {Ip} from './decorators/ip.decorator'
+import { Ip } from './decorators/ip.decorator';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(@Ip() ip: string): string {
     // console.log(ip);
     // return this.appService.getHello();
     // throw new HttpException('NotFound', HttpStatus.NOT_FOUND);
+    console.log(this.configService.get('ENVIRONMENT'));
     return this.appService.getHello();
   }
 }
