b05d9558ee043807c745f81917d546f1d1f851bc
HwangChulHee <kum15479@naver.com>
2024-07-04 16:27:42 +0900

    4-8 미들웨어를 통한 로깅처리

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import {Ip} from './decorators/ip.decorator'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Ip() ip: string): string {
+    // console.log(ip);
+    // return this.appService.getHello();
+    // throw new HttpException('NotFound', HttpStatus.NOT_FOUND);
     return this.appService.getHello();
   }
 }
